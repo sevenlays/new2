b39a1b35ab12cdb937cc425e2bbf81463bc69c55
sevenlays <7evenLays@gmail.com>
2022-03-24 14:09:54 +0200

    moviesv3.0

---
+++ js/scripttraining.js
@@ -0,0 +1,65 @@
+'use strict';
+
+// let num = 20;
+
+// function showFirstMessage (text) {
+//     console.log(text);
+//     console.log(num);
+// }
+
+// showFirstMessage('Hello world');
+// console.log(num);
+
+// console.log(calc(4,5));
+// console.log(calc(1,2));
+// console.log(calc(3,4));
+// console.log(calc(5,6));
+
+// function calc (a, b) {
+//     return (a * b);
+// }
+
+// function ret() {
+//     let num = 50;
+//     return num;
+// }
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+
+
+// const logger = function () {
+//     console.log('Hello');
+// };
+
+// logger();
+
+
+// const calc = (a, b) => {
+//     console.log("2");
+//     return(a + b);
+// };
+// console.log(calc(2,4));
+
+const str = 'teSt';
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+const fruit = "Some Fruit";
+
+console.log(fruit.indexOf("q"));
+
+const logg = 'Hello world vseh zaebal';
+
+// console.log(logg.slice(17));
+
+console.log(logg.substr(12, 11));
+
+const num = 12.32;
+
+console.log(Math.round(num));
+
+const test = '12.3px';
+// console.log(parseInt(test));
+console.log(parseFloat(test));
