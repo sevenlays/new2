b7bb7f3c0cc559146b997badf8d338b344279b01
sevenlays <7evenLays@gmail.com>
2022-04-29 21:32:58 +0300

    carresponsive.v.1.1

---
+++ carsresponsive/assets/js/main.js
@@ -0,0 +1,73 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu');
+    });
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu');
+    });
+}
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu');
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+    const header = document.getElementById('header');
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader);
+//================== POPULAR SWIPER ==============
+let swiperPopular = new Swiper(".popular__container", {
+    loop: true, 
+    spaceBetween: 24,
+    slidesPerView: 'auto',
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+    breakpoints: {
+        768: {
+          slidesPerView: 3,
+        },
+        1024: {
+          spaceBetween: 48,
+        },
+      },
+  });
+//   ============= MIXITUP FILTERS =========
+let mixerFeatured = mixitup('.featured__content', {
+    selectors: {
+        target: '.featured__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
+// Link active featured
+const linkFeatured = document.querySelectorAll('.featured__item');
+
+function activeFeatured(){
+    linkFeatured.forEach(l=> l.classList.remove('active-featured'));
+    this.classList.add('active-featured');
+}
+linkFeatured.forEach(l=> l.addEventListener('click', activeFeatured));
--- js/scripttraining.js
+++ js/scripttraining.js
@@ -1,5 +1,25 @@
-const btn = document.querySelector('button');
+const btn = document.querySelector('button'),
+    overlay = document.querySelector('.overlay');
 
-btn.onclick = function () {
-    alert('Click');
+// btn.onclick = function () {
+//     alert('Click');
+// };
+let i = 0;
+const delElement = (event)=>{
+    console.log(event.currentTarget);
+    console.log(event.type);
+    // i++;
+    // if (i == 1) {
+    //     btn.removeEventListener('click', delElement);
+    // }
 };
+
+btn.addEventListener('click', delElement);
+overlay.addEventListener('click', delElement);
+
+const link = document.querySelector('a');
+link.addEventListener('click', (event)=> {
+    event.preventDefault();
+    console.log(event.target);
+});
+
